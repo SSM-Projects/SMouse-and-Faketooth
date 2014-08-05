0103bcaabe7a0d9c1650cce8e73bf81148f98304
JHKim <kimjeongss@gmail.com>
2014-08-05 22:34:14 +0900

    Modified SystemUI package

---
+++ 01-Frameworks/frameworks/base/packages/SystemUI/src/com/android/systemui/statusbar/policy/MouseController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import java.util.ArrayList;
+
+public class MouseController extends BroadcastReceiver {
+    private static final String TAG = "StatusBar.MouseController";
+
+    private ArrayList<MouseStateChangeCallback> mChangeCallbacks =
+            new ArrayList<MouseStateChangeCallback>();
+
+    public interface MouseStateChangeCallback {
+        public void onMouseModeChanged(boolean enabled);
+    }
+
+    public MouseController(Context context) {
+        IntentFilter filter = new IntentFilter();
+        context.registerReceiver(this, filter);
+    }
+
+    public void addStateChangedCallback(MouseStateChangeCallback cb) {
+        mChangeCallbacks.add(cb);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
