a5f59fadd1479e65c6790cbba20f3547f3875f1a
JHKim <kimjeongss@gmail.com>
2014-09-26 05:33:09 +0900

    Remove unnecessary files & Update 'SMouse' frameworks/native/ part sources.

--- 01-Frameworks/frameworks/native/opengl/tools/glgen/stubs/gles11/glGetBufferPointerv.java
+++
@@ -1,7 +0,0 @@
-    // C function void glGetBufferPointerv ( GLenum target, GLenum pname, GLvoid** params )
-
-    public static native java.nio.Buffer glGetBufferPointerv(
-        int target,
-        int pname
-    );
-
--- 01-Frameworks/frameworks/native/opengl/tools/glgen/stubs/gles11/glMapBufferRange.java
+++
@@ -1,9 +0,0 @@
-    // C function GLvoid * glMapBufferRange ( GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access )
-
-    public static native java.nio.Buffer glMapBufferRange(
-        int target,
-        int offset,
-        int length,
-        int access
-    );
-
